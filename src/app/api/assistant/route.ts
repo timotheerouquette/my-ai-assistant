30046034a2119c80f63ea010a53bbac5f0309709
Timothée Rouquette <timothee.rouquette@gmail.com>
2024-04-25 14:20:13 +0200

    cc simon

--- src/app/api/assistant/route.ts
+++ src/app/api/assistant/route.ts
@@ -1,28 +1,116 @@
+import { AssistantResponse } from "ai";
 import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || "",
 });
 
-// Set the runtime to edge for best performance
-export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const homeTemperatures = {
+  bedroom: 20,
+  "home office": 21,
+  "living room": 21,
+  kitchen: 22,
+  bathroom: 23,
+};
 
 export async function POST(req: Request) {
-  const { message } = await req.json();
+  // Parse the request body
+  const input: {
+    threadId: string | null;
+    message: string;
+  } = await req.json();
 
-  const thread = await openai.beta.threads.create();
+  // Create a thread if needed
+  const threadId = input.threadId ?? (await openai.beta.threads.create({})).id;
 
-  const response = await openai.beta.threads.messages.create(thread.id, {
+  // Add a message to the thread
+  const createdMessage = await openai.beta.threads.messages.create(threadId, {
     role: "user",
-    content: message,
+    content: input.message,
   });
 
-  const stream = await openai.beta.threads.runs.stream(thread.id, {
-    assistant_id: "asst_1m6HOKnDJNcm2qz9qQTYQwO9",
-  });
+  return AssistantResponse(
+    { threadId, messageId: createdMessage.id },
+    async ({ forwardStream, sendDataMessage }) => {
+      // Run the assistant on the thread
+      const runStream = openai.beta.threads.runs.createAndStream(threadId, {
+        assistant_id:
+          process.env.ASSISTANT_ID ??
+          (() => {
+            throw new Error("ASSISTANT_ID is not set");
+          })(),
+      });
 
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+      // forward run status would stream message deltas
+      let runResult = await forwardStream(runStream);
+
+      // status can be: queued, in_progress, requires_action, cancelling, cancelled, failed, completed, or expired
+      while (
+        runResult?.status === "requires_action" &&
+        runResult.required_action?.type === "submit_tool_outputs"
+      ) {
+        const tool_outputs =
+          runResult.required_action.submit_tool_outputs.tool_calls.map(
+            (toolCall: any) => {
+              const parameters = JSON.parse(toolCall.function.arguments);
+
+              switch (toolCall.function.name) {
+                case "getRoomTemperature": {
+                  const temperature =
+                    homeTemperatures[
+                      parameters.room as keyof typeof homeTemperatures
+                    ];
+
+                  return {
+                    tool_call_id: toolCall.id,
+                    output: temperature.toString(),
+                  };
+                }
+
+                case "setRoomTemperature": {
+                  const oldTemperature =
+                    homeTemperatures[
+                      parameters.room as keyof typeof homeTemperatures
+                    ];
+
+                  homeTemperatures[
+                    parameters.room as keyof typeof homeTemperatures
+                  ] = parameters.temperature;
+
+                  sendDataMessage({
+                    role: "data",
+                    data: {
+                      oldTemperature,
+                      newTemperature: parameters.temperature,
+                      description: `Temperature in ${parameters.room} changed from ${oldTemperature} to ${parameters.temperature}`,
+                    },
+                  });
+
+                  return {
+                    tool_call_id: toolCall.id,
+                    output: `temperature set successfully`,
+                  };
+                }
+
+                default:
+                  throw new Error(
+                    `Unknown tool call function: ${toolCall.function.name}`
+                  );
+              }
+            }
+          );
+
+        runResult = await forwardStream(
+          openai.beta.threads.runs.submitToolOutputsStream(
+            threadId,
+            runResult.id,
+            { tool_outputs }
+          )
+        );
+      }
+    }
+  );
 }
