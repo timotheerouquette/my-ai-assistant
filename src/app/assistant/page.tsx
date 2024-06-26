386d1ca5f8fb22493b6dcc60b08deb0116e4fb72
Timothée Rouquette <timothee.rouquette@gmail.com>
2024-06-26 15:43:20 +0200

    ✨ assistant evolution

--- src/app/assistant/page.tsx
+++ src/app/assistant/page.tsx
@@ -1,39 +1,120 @@
 "use client";
 
 import { Message, useAssistant } from "ai/react";
+import cn from "clsx";
+import Link from "next/link";
+import { Discuss } from "react-loader-spinner";
+
+const questionsList = [
+  "Livrez-vous au Royaume-Uni?",
+  "Quels sont les avantages réservés aux clients privilège ?",
+  "Puis-je annuler une commande ?",
+  "Existe-t-il un programme de fidélité pour les bons clients ?",
+  "Quels sont les avantages du club idéal ?",
+  "Ma commande n’est pas conforme ?",
+  "Comment marche me regroupement de commandes et la livraison différée ?",
+  "D’où proviennent les vins vendus en e-cave ?",
+  "Je n’arrive pas à passer un ordre d’achat",
+  "Comment marche votre service de stockage ?",
+  "Quel plan de stockage choisir ?",
+];
 
 export default function Chat() {
-  const { status, messages, input, submitMessage, handleInputChange } =
-    useAssistant({ api: "/api/assistant" });
+  const {
+    status,
+    messages,
+    input,
+    submitMessage,
+    handleInputChange,
+    setMessages,
+    setInput,
+  } = useAssistant({ api: "/api/assistant", threadId: undefined });
 
   return (
     <div>
-      {messages.map((m: Message) => (
-        <div key={m.id}>
-          <strong>{`${m.role}: `}</strong>
-          {m.role !== "data" && m.content}
-          {m.role === "data" && (
-            <>
-              {(m.data as any).description}
-              <br />
-              <pre className={"bg-gray-200"}>
-                {JSON.stringify(m.data, null, 2)}
-              </pre>
-            </>
-          )}
-        </div>
-      ))}
+      <button
+        onClick={() => {
+          setMessages([]);
+        }}
+        className="mb-2 rounded-lg py-2 px-4 bg-red-800 text-white font-bold fixed top-4 right-4"
+      >
+        New chat
+      </button>
 
-      {status === "in_progress" && <div />}
+      <Link
+        target="_blank"
+        href="https://www.idealwine.com/fr/aide/faq"
+        className="mb-2 rounded-lg py-2 px-4 bg-red-800 text-white font-bold fixed top-16 right-4"
+      >
+        FAQ
+      </Link>
 
-      <form onSubmit={submitMessage}>
-        <input
-          disabled={status !== "awaiting_message"}
-          value={input}
-          placeholder="What is the temperature in the living room?"
-          onChange={handleInputChange}
-        />
-      </form>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+        {messages.map((message: Message) => (
+          <div
+            key={message.id}
+            className={cn(
+              "mb-2 rounded-lg py-2 px-4 w-[80%]",
+              message.role === "user"
+                ? "text-end bg-gray-300 self-end"
+                : "bg-red-800 text-white"
+            )}
+          >
+            {message.role !== "data" && message.content}
+            {message.role === "data" && (
+              <>
+                {(message.data as any).description}
+                <br />
+                <pre className={"bg-gray-200"}>
+                  {JSON.stringify(message.data, null, 2)}
+                </pre>
+              </>
+            )}
+          </div>
+        ))}
+
+        {status === "in_progress" && (
+          <Discuss
+            visible={true}
+            height="30"
+            width="30"
+            colors={["#991B1B", "#991B1B"]}
+          />
+        )}
+
+        <form onSubmit={submitMessage}>
+          <div className="fixed bottom-0 w-full max-w-md flex flex-col text-end">
+            {messages.length === 0 ? (
+              <div className="pb-3">
+                <span className="font-bold">Questions fréquentes :</span>
+                <ul className="h-16 overflow-auto">
+                  {questionsList.map((question) => (
+                    <li className="cursor-pointer text-gray-500" key={question}>
+                      <button
+                        onClick={(event) => {
+                          setInput(question);
+                        }}
+                        type="submit"
+                        className="underline"
+                      >
+                        {question}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+
+            <input
+              className="w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+              disabled={status !== "awaiting_message"}
+              value={input}
+              placeholder="Quels sont les avantages réservés aux clients privilège ?"
+              onChange={handleInputChange}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
